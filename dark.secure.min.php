03db95128b695ece9967a8f3388656ee2ff5c858
Ange SAADJIO <sanix000@yahoo.fr>
2016-10-04 21:48:00 +0200

    Create dark.secure.min.php

---
+++ dark.secure.min.php
@@ -0,0 +1,2 @@
+
+	function s($tr){return str_replace("\'","'",htmlspecialchars($tr));}function sa($tr){return str_replace("'","\'",htmlspecialchars($tr));}function a($mot){return Addslashes($mot);}function i($chiff){return intval($chiff);}function r($a,$b,$char){return s(str_replace($a,$b,$char));}
